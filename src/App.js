48ce49430985b2bfb2449fdf0798d70792df1fda
Pr-tham981 <prathamkumar2184@gmail.com>
2021-12-22 19:08:58 +0530

    myfirstcommit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Weathercard from "./component/weathercard";
+
+ import Temp from "./components/weather/temp";
+
+const App = () => {
+  return (
+    <>
+      <Weathercard />
+    </>
+  );
+};
+
+export default App;
